3a3e44ef6acb8234163facd92d7be7f2f927b501
Furkan ACAR <furkan@acar.digital>
2019-09-05 17:33:44 +0300

    Router setup.

---
+++ src/components/layout/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Layout() {
+  return (
+      <header>
+          <div className="brand"></div>
+          {/* <Nav></Nav> */}
+      </header>
+  );
+}
