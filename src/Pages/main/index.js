bb0b730e7f9f19330b45b9e2d7bc0c5bdc1376f2
Furkan ACAR <furkan@acar.digital>
2019-09-06 02:24:13 +0300

    Installed isomorphic-unfetch.
    Basically fetching data in componentDidMount. It's better to use Redux.
    
    Style improvements.

--- src/Pages/main/index.js
+++ src/Pages/main/index.js
@@ -1,45 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import fetch from 'isomorphic-unfetch';
 import './style.scss';
 import hero from '../../Assets/big-photo.png';
 import hero2x from '../../Assets/big-photo@2x.png';
 
-// const fetchPageData = () => ()
-// https://jsonplaceholder.typicode.com/photos?_limit=3
+const headings = ['Features', 'Architecture', 'Insights'];
+
+const description = `
+    Ea perspiciatis accusantium nobis pariatur.
+    Assumenda enim nisi qui enim.
+    Veniam aut sit hic soluta non debitis et officiis.
+    Omnis et maiores a id placeat veniam odit molestias.
+    Consequatur voluptas ipsa dolores dolorem et ullam qui saepe.
+    Omnis non quibusdam veniam autem sint.
+`;
+
+const fetchPageData = async () =>
+	await fetch('https://jsonplaceholder.typicode.com/photos?_limit=3')
+		.then(res => res.json())
+		.then(data => data.map(({ thumbnailUrl }) => thumbnailUrl));
 
 const MainPage = () => {
+	const [images, setImages] = useState([]);
+
+	useEffect(() => {
+		fetchPageData().then(setImages);
+	}, [images]);
+
 	return (
 		<section className="MainPage">
 			<div className="hero">
 				<img
-					srcset={`${hero} 1x, ${hero2x} 2x`}
+					srcSet={`${hero} 1x, ${hero2x} 2x`}
 					src={hero}
 					alt="Hero"
 				/>
 			</div>
 
 			<div className="features">
-                <div className="container">
-                    <div className="features__one">
-                        <div className="features__one__image">
-                            {/* <img src={} /> */}
-                        </div>
-                        <div className="features__one__heading">Feature 1</div>
-                        <p className="features__one__description">asdqweasdqweasdqweasdqwe</p>
-                    </div>
-
-                    <div className="features__one">
-                        {/* <img className="features__one__image" src={} /> */}
-                        <div className="features__one__heading">Feature 2</div>
-                        <p className="features__one__description">asdqweasdqweasdqweasdqwe</p>
-                    </div>
-
-                    <div className="features__one">
-                        {/* <img className="features__one__image" src={} /> */}
-                        <div className="features__one__heading">Feature 3</div>
-                        <p className="features__one__description">asdqweasdqweasdqweasdqwe</p>
-                    </div>
-                </div>
-            </div>
+				<div className="container">
+					{images.map((img, i) => (
+						<div className="features__one" key={i}>
+							<div className="features__one__image">
+								<img src={img} alt={headings[i]} />
+							</div>
+							<div className="features__one__heading">
+								{headings[i]}
+							</div>
+							<p className="features__one__description">
+								{description}
+							</p>
+						</div>
+					))}
+				</div>
+			</div>
 		</section>
 	);
 };
