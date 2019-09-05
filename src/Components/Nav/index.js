2ba7fd3963c602a668c2783345bddfc539e23eb5
Furkan ACAR <furkan@acar.digital>
2019-09-06 02:50:24 +0300

    Styles organized. Added nav-link__sub.

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -1,12 +1,22 @@
 import * as React from 'react';
 import NavLink from '../NavLink';
+import './style.scss';
 
 const navLinks = [
 	{ name: 'Main Page', to: '/' },
-	{ name: 'Products', to: '/products' },
+	{
+		name: 'Products',
+		to: '/products',
+		subLinks: [
+			{ name: 'On Boarding', to: 'onboarding' },
+			{ name: 'Customisations', to: 'customisations' },
+			{ name: 'Templates', to: 'templates' },
+			{ name: 'Integrations', to: 'intergations' },
+		],
+	},
 	{ name: 'Solutions', to: '/solutions' },
 	{ name: 'Services', to: '/services' },
-	{ name: 'Pricing', to: '/pricing' }
+	{ name: 'Pricing', to: '/pricing' },
 ];
 
 const Nav = () => (
--- src/Components/NavLink/index.js
+++ src/Components/NavLink/index.js
@@ -1,14 +1,25 @@
 import * as React from 'react';
 import { Link } from '@reach/router';
+import './style.scss';
 
 const setIsActive = ({ isCurrent }) => ({
 	className: isCurrent ? 'active' : '',
 });
 
-const NavLink = props => (
-	<Link {...props} getProps={setIsActive}>
-		{props.name}
-	</Link>
+const NavLink = ({ name, to, subLinks }) => (
+	<div className="nav-link">
+		<Link to={to} getProps={setIsActive}>
+			{name}
+		</Link>
+
+		{subLinks && (
+			<div className="nav-link__sub">
+				{subLinks.map((sub, i) => (
+					<NavLink {...sub} key={sub.name + i}></NavLink>
+				))}
+			</div>
+		)}
+	</div>
 );
 
 export default NavLink;
