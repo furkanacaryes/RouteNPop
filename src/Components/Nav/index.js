f4285f7f51df7713e7495fdbbb85b0450187bef3
Furkan ACAR <furkan@acar.digital>
2019-09-06 01:15:52 +0300

    Added Fonts. Edited layout paddings.

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -4,12 +4,15 @@ import NavLink from '../NavLink';
 const navLinks = [
 	{ name: 'Main Page', to: '/' },
 	{ name: 'Products', to: '/products' },
+	{ name: 'Solutions', to: '/solutions' },
+	{ name: 'Services', to: '/services' },
+	{ name: 'Pricing', to: '/pricing' }
 ];
 
 const Nav = () => (
 	<nav>
-		{navLinks.map(({ ...props }) => (
-			<NavLink {...props}></NavLink>
+		{navLinks.map(({ ...props }, i) => (
+			<NavLink {...props} key={i}></NavLink>
 		))}
 	</nav>
 );
---
+++ src/Fonts/index.js
@@ -0,0 +1,4 @@
+
+// Source Sans Pro
+export * from './SourceSansPro/SourceSansPro-Regular.ttf';
+export * from './SourceSansPro/SourceSansPro-Bold.ttf';
