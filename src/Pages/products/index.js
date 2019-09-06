130a50537e02840b7f83887c7b5e735923610fa0
Furkan ACAR <furkan@acar.digital>
2019-09-07 00:44:26 +0300

    Mocking data due to empty API response. States defined and template created for Note Component.

--- src/Pages/products/index.js
+++ src/Pages/products/index.js
@@ -1,15 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import fetch from 'isomorphic-unfetch';
 
-const fetchPageData = () =>
-	fetch('https://hastebin.com/wewumotepi.json').then(res => res.json());
+const apiResponse = {
+	text:
+		'Hello[1], I am a mock[2] response because real API[3] does NOT respond.',
+	notes: {
+		1: 'Bonjour',
+		2: 'Not actual, made up',
+		3: 'Application Programming Interface. Meant a web service here.',
+	},
+};
 
-const Products = () => (
-    // const [note, setNote] = useState('');
 
-	<section className="Products">
-		<div>Hello!</div>
-	</section>
-);
+
+const Products = () => {
+	const [{ notes, splittedText }, setData] = useState();
+
+	const [activeNote, setActiveNote] = useState();
+
+	useEffect(() => {
+		fetchPageData().then(data => {
+			const viewData = prepareData(data);
+			setData(viewData);
+		});
+	}, [text]);
+
+	return (
+		<section className="Products">
+			<div>Hello!</div>
+
+			<div className="Note">
+				{/* <div>{data.text[i]}</div>
+				<div className="foot-note" onClick={() => setActiveNote(i)}>
+					<div className="pop-over">{data.footNotes[i]}</div>
+				</div> */}
+			</div>
+		</section>
+	);
+};
 
 export default Products;
