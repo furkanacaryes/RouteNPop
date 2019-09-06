1eeb9237c73606a1b224b535241b30b708578a89
Furkan ACAR <furkan@acar.digital>
2019-09-07 01:57:22 +0300

    Styled Note Component. Implemented click logic.

--- src/Pages/products/index.js
+++ src/Pages/products/index.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import fetch from 'isomorphic-unfetch';
+import React, { useState, useEffect, useRef } from 'react';
+// import fetch from 'isomorphic-unfetch';
+import './style.scss';
 
 const apiResponse = {
 	text:
@@ -11,29 +12,75 @@ const apiResponse = {
 	},
 };
 
+const FOOTNOTE_REGEXP = new RegExp(/\[[0-9]\]/g);
 
+const fetchPageData = () =>
+	// fetch('https://hastebin.com/wewumotepi.json').then(res => res.json());
+	Promise.resolve(apiResponse);
+
+const alignNoteIndexes = (text, notes) =>
+	text.match(FOOTNOTE_REGEXP).map(placeholder => {
+		const noteIndex = placeholder[1]; // '[', 'here', ']'
+		return notes[noteIndex];
+	});
+
+const prepareData = ({ text, notes }) => ({
+	splittedText: text.split(FOOTNOTE_REGEXP),
+	notes: alignNoteIndexes(text, notes),
+});
 
 const Products = () => {
-	const [{ notes, splittedText }, setData] = useState();
+	const [{ notes, splittedText }, setData] = useState({});
 
 	const [activeNote, setActiveNote] = useState();
 
+	const footNoteRef = useRef();
+
+	const getStatefulClass = noteIndex =>
+		activeNote === noteIndex ? 'active' : '';
+
+	const getInitialProps = () =>
+		fetchPageData().then(data => setData(prepareData(data)));
+
+	const outsideClickListener = [
+		'click',
+		({ target }) => {
+			if (target !== footNoteRef.current) {
+				setActiveNote(null);
+			}
+		},
+	];
+
 	useEffect(() => {
-		fetchPageData().then(data => {
-			const viewData = prepareData(data);
-			setData(viewData);
-		});
-	}, [text]);
+		getInitialProps();
+
+		document.addEventListener(...outsideClickListener);
+
+		return () => document.removeEventListener(...outsideClickListener);
+	}, [splittedText, outsideClickListener]);
 
 	return (
 		<section className="Products">
-			<div>Hello!</div>
-
-			<div className="Note">
-				{/* <div>{data.text[i]}</div>
-				<div className="foot-note" onClick={() => setActiveNote(i)}>
-					<div className="pop-over">{data.footNotes[i]}</div>
-				</div> */}
+			<div className="Note shadow-radius-box">
+				{splittedText &&
+					splittedText.map((partialText, i) => (
+						<div key={i} className="partial-text">
+							{partialText}
+							<div
+								className="foot-note"
+								ref={footNoteRef}
+								onClick={() => setActiveNote(i)}
+							>
+								<div
+									className={`pop-over shadow-radius-box ${getStatefulClass(
+										i,
+									)}`}
+								>
+									{notes[i]}
+								</div>
+							</div>
+						</div>
+					))}
 			</div>
 		</section>
 	);
