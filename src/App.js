5eb5b61e365cb318c6ee3ca9c5b67b58a8871f7e
Furkan ACAR <furkan@acar.digital>
2019-09-08 23:45:19 +0300

    Created Context.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Router } from '@reach/router';
 import './App.scss';
+import StateContext from './stateContext';
 import Layout from './Components/Layout';
 import MainPage from './Pages/main';
 import Products from './Pages/products';
 
 const App = () => (
-	<Router>
-		<Layout path="/">
-			<MainPage path="/" default></MainPage>
-			<Products path="/products"></Products>
-		</Layout>
-	</Router>
+	<StateContext.Provider value={useState()}>
+		<Router>
+			<Layout path="/">
+				<MainPage path="/" default></MainPage>
+				<Products path="/products"></Products>
+			</Layout>
+		</Router>
+	</StateContext.Provider>
 );
 
 export default App;
